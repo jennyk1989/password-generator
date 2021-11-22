d0353180794a70ebee6651414e85189c387c1c9f
jennyk1989 <jennifer0keene@gmail.com>
2021-11-21 20:42:41 -0600

    fixed character type validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@
 //define variables
 var generateBtn = document.querySelector("#generate");
 var passLength;
-var lowercasePass = "abcdefghijklmnopqrstuvwxyz";
-var uppercasePass = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericPass = "0123456789";
-var specialPass = "!@#$%^&*()_+=-";
+var lowercasePass = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercasePass = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var numericPass = "0123456789".split("");
+var specialPass = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("");
 
 //password generator function
 function generatePassword (){
@@ -13,18 +13,19 @@ function generatePassword (){
   var randomPass = "";
 
   //choose length of password
-  passLength = prompt("Choose the number of characters in your password (>=8 & <128 characters)");
-  if (passLength < 8 || passLength > 128) {
+  passLength = prompt("Choose the number of characters in your password.");
+  if (passLength < 8 || passLength > 128 || isNaN(passLength)) {
     alert ("Your password must be 8 to 128 characters long.");
   }
   //other password criteria
-  else {
+  
+  
     //lowercase letters? 
     if(confirm("Will your password contain lowercase letters?")) {
       charChoosen = charChoosen.concat(lowercasePass);
     }
     //uppercase letters?
-    if(confirm("Will your password contain uppercase letter?")) {
+    if(confirm("Will your password contain uppercase letters?")) {
       charChoosen = charChoosen.concat(uppercasePass);
     }
     //numbers?
@@ -35,21 +36,28 @@ function generatePassword (){
     if(confirm("Will your password contain special characters?")) {
       charChoosen = charChoosen.concat(specialPass);
     }
+  
     //validate at least one character type is selected
-    if (lowercasePass || uppercasePass || numericPass || specialPass) {
-      alert("congrats");
-    } else { 
-      alert("You much choose at least one character type for your password")}
-  }
+    if (charChoosen.length === 0) {
+      alert("You much choose at least one character type for your password.");
+      return generatePassword();
+    }
+
+    //for loop that generates password based on selected criteria
+    for(var i = 0; i < passLength; i++) {
+      var password = Math.floor(Math.random() * charChoosen.length);
+      randomPass += charChoosen[password];
+    }
+  
+  return randomPass;
 }
 
 
 // Write password to the #password input
 function writePassword() {
-  var passwordChoosen  = "";
-  passwordChoosen = generatePassword();
+  var password  =  generatePassword();
   var passwordText = document.querySelector("#password");
-  passwordText.value = passwordChoosen;
+  passwordText.value = password;
 
 }
 
